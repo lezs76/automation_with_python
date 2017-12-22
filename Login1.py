cf79415353394045a0b559aebe78596e9de0f6a9
lezs76 <lezs76@gmail.com>
2017-12-22 09:33:36 -0800

    commit3

--- Login1.py
+++ Login1.py
@@ -1,14 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import selenium.webdriver.common.keys
+username = 'xxxx@xxxx.com'
+password = 'xxxxx!1'
 
-username = 'lezs76@gmail.com'
-password = 'Raining!1'
-
-class(object)
-
+class login(object):
+  
+  
 #username
-driver.find_element_by_name("username").send_keys(username)
+  def UserName(self,username):
+  driver.find_element_by_name("username").send_keys(username)
 #Password
-driver.find_element_by_name("password").send_keys(password)
-#Click Login
+  def Password(self,password):
+  driver.find_element_by_name("password").send_keys(password)
+
