e1536d249047e5e5fe154872f7c3ddad250a639c
lezs76 <lezs76@gmail.com>
2017-12-22 09:20:44 -0800

    First commit

---
+++ Login1.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver.common.keys
+
+username = 'lezs76@gmail.com'
+password = 'Raining!1'
+
+#username
+driver.find_element_by_name("username").send_keys(username)
+#Password
+driver.find_element_by_name("password").send_keys(password)
+#Click Login
